d225e9b7d6786ce3dbecbb9948f9110982feb21b
KoIXozNiK <vasylyk29@gmail.com>
2019-01-08 23:55:40 +0200

    timer v 1.0.0

---
+++ js/script.js
@@ -0,0 +1,53 @@
+window.addEventListener('DOMContentLoaded', function () {
+    'use strict';
+
+    //TIMER
+    let deadline = '2019-01-10';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t / 1000) % 60),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            hours = Math.floor((t / (1000 * 60 * 60)));
+
+        return {
+            "total": t,
+            "second": seconds,
+            "minutes": minutes,
+            "hours": hours,
+        };
+    }
+
+    function setCloak(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(undateCloack, 1000);
+
+
+        function undateCloack() {
+            let t = getTimeRemaining(endtime);
+
+            function addZero(num) {
+                if (num <= 9) {
+                    return "0" + num;
+                } else return num;
+            }
+
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.second);
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                hours.textContent = '00';
+                minutes.textContent = '00';
+                seconds.textContent = '00';
+            }
+        }
+    }
+    setCloak('timer', deadline);
+
+
+});
